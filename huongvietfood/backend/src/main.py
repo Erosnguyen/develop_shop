503cc11e4ce3d9ad3ba3e11a122fdf74c5208e47
Erosnguyen <cuong123go@gmail.com>
2024-04-16 15:57:53 +0700

    init project

---
+++ huongvietfood/backend/src/main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.routers.healthcheck.router import router as healthcheck_router
+
+load_dotenv()
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(healthcheck_router)
