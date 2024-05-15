0dd61f87026dce441fa741bf19edfd9715b35450
HNam <huydre37@gmail.com>
2024-05-15 12:11:47 +0700

    feat: add hero section

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ import { useLocation } from "react-router-dom";
 import { CartIcon } from "../../assets/CartIcon"
 import { Logo } from "../../assets/Logo";
 
-const NavBar = ({ setShowLogin }) => {
+const NavBar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
 
@@ -54,8 +54,8 @@ const NavBar = ({ setShowLogin }) => {
       id: 2,
     },
     {
-      name: "About",
-      link: "/about",
+      name: "Liên hệ",
+      link: "/contact",
       id: 3,
     },
   ];
@@ -68,88 +68,6 @@ const NavBar = ({ setShowLogin }) => {
     }
   }, [token]);
   return (
-    // <div className="navbar pt-5 justify-between flex items-center">
-    //   {/* <img src={assets.logo} alt="" className="w-[150px]" /> */}
-    //   <div className="logo lg:w-[140px] md:w-[120px]">
-    //     <Link to="/">
-    //       <h1>HUONG VIET</h1>
-    //     </Link>
-    //   </div>
-    //   <ul className="navbar-menu flex list-none gap-5 text-[#49557e] text-lg xl:gap-[20px] lg:gap-[15px] lg:text-[16px] max-md:hidden max-lg:hidden scroll-smooth">
-    //     <a
-    //       href="/"
-    //       onClick={() => setMenu("home")}
-    //       className={
-    //         menu === "home"
-    //           ? "active:pb-[2px] border-b-2 border-[#49557e] cursor-pointer scroll-smooth"
-    //           : "cursor-pointer"
-    //       }
-    //     >
-    //       Home
-    //     </a>
-    //     <a
-    //       href="#explore-menu"
-    //       onClick={() => setMenu("menu")}
-    //       className={
-    //         menu === "menu"
-    //           ? "active:pb-[2px] border-b-2 border-[#49557e] cursor-pointer"
-    //           : "cursor-pointer"
-    //       }
-    //     >
-    //       Menu
-    //     </a>
-    //     <a
-    //       href="#app-download"
-    //       onClick={() => setMenu("mobile")}
-    //       className={
-    //         menu === "mobile"
-    //           ? "active:pb-[2px] border-b-2 border-[#49557e] cursor-pointer"
-    //           : "cursor-pointer"
-    //       }
-    //     >
-    //       Mobile
-    //     </a>
-    //     <a
-    //       href="#footer"
-    //       onClick={() => setMenu("contact-us")}
-    //       className={
-    //         menu === "contact-us"
-    //           ? "active:pb-[2px] border-b-2 border-[#49557e] cursor-pointer"
-    //           : "cursor-pointer"
-    //       }
-    //     >
-    //       Contact us
-    //     </a>
-    //   </ul>
-    //   <div className="navbar-right flex items-center gap-10 max-xl:gap-[30px] max-lg:gap-5">
-    //     <img
-    //       className="max-lg:w-[22px] max-md:w-5"
-    //       src={assets.search_icon}
-    //       alt=""
-    //     />
-    //     <div className="navbar-search-icon relative max-lg:w-[22px] max-md:w-5">
-    //       <Link to={"/cart"}>
-    //         <img src={assets.basket_icon} alt="" />
-    //       </Link>
-    //       <div className="dot absolute min-w-[10px] min-h-[10px] bg-amber-700 rounded-[5px] -top-2 -right-2"></div>
-    //     </div>
-    //     {
-    //       isLoggedIn ?
-    //         <button
-    //           onClick={() => handleLogout()}
-    //           className="bg-transparent text-base text-[#49557e] border border-solid border-amber-700  rounded-[50px] cursor-pointer hover:bg-[#fff4f2] text-center px-[30px] py-[10px] max-xl:py-[8px] max-xl:px-[25px] max-lg:px-[20px] max-lg:py-[7px] lg:text-[15px]"
-    //         >
-    //           Logout
-    //         </button> :
-    //         <button
-    //           onClick={() => setShowLogin(true)}
-    //           className="bg-transparent text-base text-[#49557e] border border-solid border-amber-700  rounded-[50px] cursor-pointer hover:bg-[#fff4f2] text-center px-[30px] py-[10px] max-xl:py-[8px] max-xl:px-[25px] max-lg:px-[20px] max-lg:py-[7px] lg:text-[15px]"
-    //         >
-    //           sign in
-    //         </button>
-    //     }
-    //   </div>
-    // </div>
     <>
       <Navbar >
         <NavbarContent justify="start">
@@ -197,7 +115,7 @@ const NavBar = ({ setShowLogin }) => {
                   as="button"
                   className="transition-transform"
                   color="secondary"
-                  name="Jason Hughes"
+                  name="User"
                   size="sm"
                   src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
                 />
@@ -205,7 +123,7 @@ const NavBar = ({ setShowLogin }) => {
               <DropdownMenu aria-label="Profile Actions" variant="flat">
                 <DropdownItem key="profile" className="h-14 gap-2">
                   <p className="font-semibold">Signed in as</p>
-                  <p className="font-semibold">zoey@example.com</p>
+                  <p className="font-semibold">user@example.com</p>
                 </DropdownItem>
                 <DropdownItem onClick={handleLogout} key="logout" color="danger">
                   Log Out
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,13 +3,15 @@ import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
+import { HeroSection } from "../../components/HeroSection/HeroSection";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
 
   return (
     <div className="">
-      <Header />
+      <HeroSection  />
+      {/* <Header /> */}
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
       <AppDownload />
