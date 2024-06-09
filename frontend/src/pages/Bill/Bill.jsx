7bbfe281ae729123382511b0e32227c37247bffd
HNam <huydre37@gmail.com>
2024-06-10 01:20:04 +0700

    update  bill

--- frontend/src/pages/Bill/Bill.jsx
+++ frontend/src/pages/Bill/Bill.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { StoreContext } from "../../context/StoreContext";
-import { getMedia, getVariantPrice } from "../../lib/utils";
+import { getMedia, getOptionName, getVariantPrice } from "../../lib/utils";
 import {
   getUserOrder,
   handleAddOrder,
@@ -51,7 +51,7 @@ const Bill = ({ product }) => {
     setState((prev) => ({
       ...prev,
       [name]: value,
-    }));  
+    }));
   };
 
   const statusColorMap = {
@@ -106,53 +106,63 @@ const Bill = ({ product }) => {
   const handleOrder = async (e) => {
     e.preventDefault();
     try {
-      if (isUser) {
-        // xoá order cũ với status pending
-        const listOrderPending = listYourOrders.filter(
-          (it) => it.status === "pending"
-        );
-        listOrderPending.forEach((it) => {
-          handleDeleteOrder(it.order_id);
-        });
-        // thêm order mới
-        const convertData = {
-          order: {
-            items: cartItems,
-          },
-          address: {
-            street: state?.street || "",
-            city: state?.city || "",
-            state: state?.state || "",
-            country: state?.country || "",
-          },
-        };
-        await handleAddOrder(convertData);
-        // xoá cart
-        localStorage.removeItem("cartItems");
-        // Lấy lại order
-        handleGetUserOrder();
-        //Update status
-        const order = listYourOrders.filter((it) => it.status === "pending");
-        await handleProcessingOrder(order[0].order_id);
-        toast.success("Order successfully!");
-        window.location = "/bill";
+      if (
+        state?.street == null ||
+        state?.city == null ||
+        state?.state == null ||
+        state?.country == null
+      ) {
+        toast.error("Please fill in all the information!");
+        return;
       } else {
-        const convertData = {
-          items: cartItems,
-          address: {
-            street: state?.street || "",
-            city: state?.city || "",
-            state: state?.state || "",
-            country: state?.country || "",
-          },
-          first_name: state?.first_name || "",
-          last_name: state?.last_name || "",
-          email: state?.email || "",
-        };
-        // console.log(convertData)
-        await handleGuestOrder(convertData);
-        localStorage.removeItem("cartItems");
-        toast.success("Order successfully!");
+        if (isUser) {
+          // xoá order cũ với status pending
+          const listOrderPending = listYourOrders.filter(
+            (it) => it.status === "pending"
+          );
+          listOrderPending.forEach((it) => {
+            handleDeleteOrder(it.order_id);
+          });
+          // thêm order mới
+          const convertData = {
+            order: {
+              items: cartItems,
+            },
+            address: {
+              street: state?.street || "",
+              city: state?.city || "",
+              state: state?.state || "",
+              country: state?.country || "",
+            },
+          };
+          await handleAddOrder(convertData);
+          // xoá cart
+          localStorage.removeItem("cartItems");
+          // Lấy lại order
+          handleGetUserOrder();
+          //Update status
+          const order = listYourOrders.filter((it) => it.status === "pending");
+          await handleProcessingOrder(order[0].order_id);
+          toast.success("Order successfully!");
+          window.location = "/bill";
+        } else {
+          const convertData = {
+            items: cartItems,
+            address: {
+              street: state?.street || "",
+              city: state?.city || "",
+              state: state?.state || "",
+              country: state?.country || "",
+            },
+            first_name: state?.first_name || "",
+            last_name: state?.last_name || "",
+            email: state?.email || "",
+          };
+          // console.log(convertData)
+          await handleGuestOrder(convertData);
+          localStorage.removeItem("cartItems");
+          toast.success("Order successfully!");
+        }
       }
     } catch (error) {
       toast.error("Order failed!");
@@ -185,19 +195,59 @@ const Bill = ({ product }) => {
             Billing Information
           </h2>
           <form action="" className="flex flex-col gap-4">
-            {
-              isUser == false && (
-                <>
-                  <Input onChange={handleChange} isRequired name="first_name" type="text" label="First Name" />
-                  <Input onChange={handleChange} isRequired name="last_name" type="text" label="Last Name" />
-                  <Input onChange={handleChange} isRequired name="email" type="email" label="Email" />
-                </>
-              )
-            }
-            <Input onChange={handleChange} isRequired name="street" type="text" label="Street" />
-            <Input onChange={handleChange} isRequired name="city"  type="text" label="City" />
-            <Input onChange={handleChange} isRequired name="state" type="text" label="State" />
-            <Input onChange={handleChange} isRequired name="country" type="text" label="Country" />
+            {isUser == false && (
+              <>
+                <Input
+                  onChange={handleChange}
+                  isRequired
+                  name="first_name"
+                  type="text"
+                  label="First Name"
+                />
+                <Input
+                  onChange={handleChange}
+                  isRequired
+                  name="last_name"
+                  type="text"
+                  label="Last Name"
+                />
+                <Input
+                  onChange={handleChange}
+                  isRequired
+                  name="email"
+                  type="email"
+                  label="Email"
+                />
+              </>
+            )}
+            <Input
+              onChange={handleChange}
+              isRequired
+              name="street"
+              type="text"
+              label="Street"
+            />
+            <Input
+              onChange={handleChange}
+              isRequired
+              name="city"
+              type="text"
+              label="City"
+            />
+            <Input
+              onChange={handleChange}
+              isRequired
+              name="state"
+              type="text"
+              label="State"
+            />
+            <Input
+              onChange={handleChange}
+              isRequired
+              name="country"
+              type="text"
+              label="Country"
+            />
             <RadioGroup label="Payment Method">
               <Radio value="bank">Bank Transfer</Radio>
               <Radio value="cash">Pay cash upon delivery</Radio>
@@ -217,13 +267,19 @@ const Bill = ({ product }) => {
                     style={{
                       backgroundImage: `url(${
                         item?.product?.media
-                          ? getMedia(item?.product?.product_id ,item?.product?.media[0]?.src)
+                          ? getMedia(
+                              item?.product?.product_id,
+                              item?.product?.media[0]?.src
+                            )
                           : "src/assets/No_Image.png"
                       })`,
                     }}
                   />
                   <div>
-                    <a href={`/product/${item.product.product_id}`} className="font-medium text-foreground underline-offset-4 hover:underline hover:opacity-80 transition-opacity cursor-pointer">
+                    <a
+                      href={`/product/${item.product.product_id}`}
+                      className="font-medium text-foreground underline-offset-4 hover:underline hover:opacity-80 transition-opacity cursor-pointer"
+                    >
                       {item.product.product_name}
                     </a>
                     <p></p>
@@ -267,7 +323,7 @@ const Bill = ({ product }) => {
           List your orders
         </h2>
 
-        <Table>
+        {/* <Table>
           <TableHeader columns={columns}>
             {(column) => (
               <TableColumn key={column.key}>{column.label}</TableColumn>
@@ -298,7 +354,98 @@ const Bill = ({ product }) => {
                 );
               })}
           </TableBody>
-        </Table>
+        </Table> */}
+        <div className="">
+          {listYourOrders
+            ?.filter((it) => it.status !== "pending")
+            .reverse()
+            .map((order, index) => (
+              <div key={index} className="mb-4 shadow-md p-4">
+                <div className="flex justify-end space-x-4">
+                  <div>
+                    {order.address.street !== ""
+                      ? order.address.street +
+                        ", " +
+                        order.address.city +
+                        ", " +
+                        order.address.state +
+                        ", " +
+                        order.address.country +
+                        " | "
+                      : ""}
+                  </div>
+                  <div>
+                    {/* <p className="uppercase">{order.status}</p> */}
+                    <Chip
+                      className="uppercase"
+                      color={statusColorMap[order?.status]}
+                      size="sm"
+                      variant="flat"
+                    >
+                      {order?.status}
+                    </Chip>
+                  </div>
+                </div>
+                {order?.items?.map((item, index) => (
+                  <div key={index} className="items-center gap-2 w-full">
+                    <div className="flex items-center gap-2 w-full p-2">
+                      <div className="flex items-center gap-2 w-full">
+                        <div
+                          className={`bg-cover bg-center rounded-xl w-20 h-20 cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${
+                              item?.product?.media
+                                ? getMedia(
+                                    item?.product?.product_id,
+                                    item?.product?.media[0]?.src
+                                  )
+                                : "src/assets/No_Image.png"
+                            })`,
+                          }}
+                        />
+                        <div>
+                          <a
+                            href={`/product/${item.product.product_id}`}
+                            className="font-medium text-foreground underline-offset-4 hover:underline hover:opacity-80 transition-opacity cursor-pointer"
+                          >
+                            {item.product.product_name}
+                          </a>
+                          {item?.product?.options.length > 0 && (
+                            <p className="text-sm">
+                              Variantion :
+                              {getOptionName(
+                                item?.product?.options,
+                                item.product.variants
+                              )}
+                            </p>
+                          )}
+                          <p className="font-medium text-foreground">
+                            $
+                            {getVariantPrice(
+                              item.product.variants,
+                              item.variant_product_id
+                            )}{" "}
+                            <span className="text-gray-400 font-normal">
+                              x {item.quantity}
+                            </span>{" "}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <Divider orientation="horzital" />
+                  </div>
+                ))}
+                <div className="flex justify-end mt-4">
+                  <div className="flex space-x-4 items-center">
+                    <p>Order Total:</p>
+                    <p className="text-xl font-semibold">
+                      ${order.total_price}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </>
   );
